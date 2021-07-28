0a05226f43e08ba7228d17258751b81113603fcd
RuslanHizhytskyi <r.hizhytskyi@gmail.com>
2021-07-28 19:21:39 +0300

    add character component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,28 +2,19 @@ import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
-import ItemList from '../itemList';
-import CharDetails from '../charDetails';
 import ErrorMessage from "../errorMessage/errorMessage";
+import CharacterPage from "../characterPage";
 
 
 export default class App extends Component {
     state = {
         showRandomChar: true,
-        selectedChar: null,
         error: false
     }
 
     componentDidCatch(error, errorInfo) {
-        console.log('error');
         this.setState({
             error: true
-        })
-    }
-
-    onCharSelected = (id) => {
-        this.setState({
-            selectedChar: id
         })
     }
 
@@ -57,14 +48,7 @@ export default class App extends Component {
                             >Toggle random character</button>
                         </Col>
                     </Row>
-                    <Row>
-                        <Col md='6'>
-                            <ItemList onCharSelected = {this.onCharSelected}/>
-                        </Col>
-                        <Col md='6'>
-                            <CharDetails charId={this.state.selectedChar}/>
-                        </Col>
-                    </Row>
+                    <CharacterPage/>
                 </Container>
             </>
         );
